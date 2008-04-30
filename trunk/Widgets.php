05f6c65b7eabb450ee9cf743aca07c471a828716
sergey.chernyshev <sergey.chernyshev@bf13d20e-8845-0410-a3e1-a3a012df6b66>
2008-04-30 18:20:08 +0000

    Added extension credentials.

--- trunk/Widgets.php
+++ trunk/Widgets.php
@@ -7,6 +7,14 @@
  * @version $Id$
  */
 
+$wgExtensionCredits['parserhook'][] = array(
+        'name' => 'Widgets',
+        'description' => 'Allows wiki administrators to add free-form widgets to wiki by just editing pages within Widget namespace. Originally developed for [http://www.ardorado.com Ardorado.com]',
+	'version' => 0.6,
+        'author' => '[http://www.sergeychernyshev.com Sergey Chernyshev] (for [http://www.semanticcommunities.com Semantic Communities LLC.])',
+        'url' => 'http://www.mediawiki.org/wiki/Extension:Widgets'
+);
+
 // Initialize Smarty
 
 require "$IP/extensions/Widgets/smarty/Smarty.class.php";
